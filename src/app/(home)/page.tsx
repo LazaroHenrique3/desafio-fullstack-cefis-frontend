49d80a22427b0320b2999f57a24160d5fa0e273e
Lazaro Henrique <lazarohenriquefernandeslima@hotmail.com>
2023-11-27 18:31:51 -0300

    feat: Criando a página de Home

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,13 @@
+import { Box, Typography} from '@mui/material'
+
+const Home = () => {
+    return ( 
+        <Box width='100%' textAlign='center' paddingTop={5}>
+            <Typography variant='h3'>
+                Seja bem vindo(a)
+            </Typography>
+        </Box>
+    )
+}
+ 
+export default Home
