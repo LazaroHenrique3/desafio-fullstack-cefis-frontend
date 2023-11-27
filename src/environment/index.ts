511bc3e2a2c6d246aec6e61072ec12cf012099c4
Lazaro Henrique <lazarohenriquefernandeslima@hotmail.com>
2023-11-27 18:17:53 -0300

    feat: Criando um arquivo com alguns valores globais que pdoem ser usados posteriormente

---
+++ src/environment/index.ts
@@ -0,0 +1,14 @@
+export const EnvironmentValues = {
+    /**
+    * Define a quantidade linhas a ser carregada por padrão nas listagens.
+    */
+    LINE_LIMIT: 5,
+    /**
+    * Placeholder exibido nos inputs de pesquisa.
+    */
+    SEARCH_INPUT: 'Pesquisar...',
+    /**
+    * Texto exibido quando nenhum registro é encontrado..
+    */
+    EMPTY_LISTING: 'Nenhum registro encontrado.',
+}
