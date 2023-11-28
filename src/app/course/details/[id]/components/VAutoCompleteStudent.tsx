30e34fce26a09d007942c5aba8f6ba60fdf80c13
Lazaro Henrique <lazarohenriquefernandeslima@hotmail.com>
2023-11-28 18:41:42 -0300

    feat: Criando o auto complete de Alunos  para a criação de Questions

---
+++ src/app/course/details/[id]/components/VAutoCompleteStudent.tsx
@@ -0,0 +1,91 @@
+import { useEffect, useMemo, useState } from 'react'
+import { Autocomplete, CircularProgress, TextField } from '@mui/material'
+
+import { toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+
+import { useField } from '@unform/core'
+import { UserService } from '@/services/api/user/UserService'
+
+type TVAutoCompleteTeacherOption = {
+    id: number
+    label: string
+}
+
+interface VAutoCompleteTeacherProps {
+    isExternalLoading: boolean
+}
+
+export const VAutoCompleteStudent: React.FC<VAutoCompleteTeacherProps> = ({ isExternalLoading = false }) => {
+    const { fieldName, registerField, defaultValue, error, clearError } = useField('idStudent')
+
+
+    const [selectedId, setSelectedId] = useState<number | undefined>(defaultValue)
+
+    const [options, setOptions] = useState<TVAutoCompleteTeacherOption[]>([])
+    const [isLoading, setIsLoading] = useState(false)
+
+    //Conectando de fato com o formulário
+    useEffect(() => {
+        registerField({
+            name: fieldName,
+            getValue: () => selectedId,
+            setValue: (_, newSelectedId) => setSelectedId(newSelectedId)
+        })
+    }, [registerField, fieldName, selectedId])
+
+    useEffect(() => {
+        setIsLoading(true)
+
+        const fetchData = async () => {
+            const result = await UserService.listUser(1, '', 'desc', true, 'STUDENT')
+            setIsLoading(false)
+
+            if (result instanceof Error) {
+                toast.error(result.message)
+                return
+            }
+
+            setOptions(result.data.map(teacher => ({ id: teacher.id, label: teacher.name })))
+        }
+
+        fetchData()
+
+    }, [])
+
+    const autoCompleteSelectedOption = useMemo(() => {
+        if (!selectedId) return null
+
+        const selectedOption = options.find(option => option.id === selectedId)
+        if (!selectedOption) return null
+
+        return selectedOption
+    }, [selectedId, options])
+
+    return (
+        <Autocomplete
+            openText='Abrir'
+            closeText='Fechar'
+            noOptionsText='Sem opções'
+            loadingText='Carregando...'
+
+            disablePortal
+            value={autoCompleteSelectedOption}
+
+            loading={isLoading}
+            disabled={isExternalLoading}
+            popupIcon={(isExternalLoading || isLoading) ? <CircularProgress size={28} color='secondary' /> : undefined}
+            onChange={(_, newValue) => { setSelectedId(newValue?.id); clearError() }}
+            options={options}
+            renderInput={(params) => (
+                <TextField
+                    {...params}
+
+                    label='Aluno'
+                    error={!!error}
+                    helperText={error}
+                />
+            )}
+        />
+    )
+}
