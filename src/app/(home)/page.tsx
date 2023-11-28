1bb512ecae6eb32d7125a98d319518d7fd2e70e7
Lazaro Henrique <lazarohenriquefernandeslima@hotmail.com>
2023-11-28 18:42:16 -0300

    feat: Ajustando o texto da Home

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,13 +1,14 @@
-import { Box, Typography} from '@mui/material'
+import { Box, Typography } from '@mui/material'
 
 const Home = () => {
-    return ( 
+    return (
         <Box width='100%' textAlign='center' paddingTop={5}>
-            <Typography variant='h3'>
-                Seja bem vindo(a)
+            <Typography variant='h5'>
+                BEM-VINDO AO FUTURO DO CONHECIMENTO.
             </Typography>
+
         </Box>
     )
 }
- 
+
 export default Home
