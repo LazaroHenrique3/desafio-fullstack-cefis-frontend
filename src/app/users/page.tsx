260a8dc3744dfa61d3390be1de302110b69319ae
Lazaro Henrique <lazarohenriquefernandeslima@hotmail.com>
2023-11-28 09:40:31 -0300

    refactor: Refatorando a página de Usuarios

--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -6,37 +6,20 @@ import { toast } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 //Material 
 import {
-    Table,
-    TableBody,
-    TableContainer,
-    TableHead,
-    TableRow,
-    Paper,
-    IconButton,
-    TableFooter,
-    Pagination,
     Button,
 } from '@mui/material'
-//Icons
-import EditIcon from '@mui/icons-material/Edit'
-import DeleteIcon from '@mui/icons-material/Delete'
-//Componentes estilizados
-import {
-    StyledTableCell,
-    StyledTableRow
-} from '@/components/styledComponents/styledListComponents'
 //Services
 import {
     IListUser,
     UserService
 } from '@/services/api/user/UserService'
 import { Loading } from '@/components/loading'
-import { EnvironmentValues } from '@/environment'
 
 import { UseHandleUser } from './hooks/customHooks'
 import { BasePageLayout } from '../BasePageLayout'
 import { AddCircleOutline } from '@mui/icons-material'
-import { convertUserRole } from '@/utils/convertUserRole'
+import { NoRegistersList } from '@/components/noRegistersList'
+import { UserTable } from './components/userTable'
 
 const Users = () => {
     const [rows, setRows] = useState<IListUser[]>([])
@@ -68,67 +51,32 @@ const Users = () => {
 
     return (
         <BasePageLayout
-            title='Usuários'
+            title="Usuários"
             linkButton={
                 <Button
                     variant="contained"
-                    color='secondary'
+                    color="secondary"
                     sx={{ width: '100px' }}
                     startIcon={<AddCircleOutline />}
                     onClick={() => router.push('/user/new')}
                 >
                     Novo
                 </Button>
-            }>
-
-            {(isLoading) ? (
+            }
+        >
+            {isLoading ? (
                 <Loading />
+            ) : totalCount === 0 ? (
+                <NoRegistersList />
             ) : (
-                <TableContainer component={Paper}>
-                    <Table sx={{ minWidth: 300 }} aria-label="customized table">
-                        <TableHead>
-                            <TableRow>
-                                <StyledTableCell size='small'>ID</StyledTableCell>
-                                <StyledTableCell size='small' align="left">Nome</StyledTableCell>
-                                <StyledTableCell size='small' align="left">Tipo</StyledTableCell>
-                                <StyledTableCell size='small' align="left">Ações</StyledTableCell>
-                            </TableRow>
-                        </TableHead>
-                        <TableBody>
-                            {rows.map((row) => (
-                                <StyledTableRow key={row.id}>
-                                    <StyledTableCell size='small' component="th" scope="row">
-                                        #{row.id}
-                                    </StyledTableCell>
-                                    <StyledTableCell size='small' align="left">{row.name}</StyledTableCell>
-                                    <StyledTableCell size='small' align="left">{convertUserRole(row.role)}</StyledTableCell>
-                                    <StyledTableCell size='small' align="left">
-                                        <IconButton color='error' onClick={() => handleDelete(row.id, row.name)}>
-                                            <DeleteIcon />
-                                        </IconButton>
-
-                                        <IconButton color='primary' onClick={() => router.push(`/user/${row.id}`)}>
-                                            <EditIcon />
-                                        </IconButton>
-                                    </StyledTableCell>
-                                </StyledTableRow>
-                            ))}
-                        </TableBody>
-                        <TableFooter>
-                            {(totalCount > 0 && totalCount > EnvironmentValues.LINE_LIMIT) && (
-                                <TableRow>
-                                    <StyledTableCell size='small' colSpan={11}>
-                                        <Pagination
-                                            page={page}
-                                            count={Math.ceil(totalCount / EnvironmentValues.LINE_LIMIT)}
-                                            onChange={(e, newPage) => setPage(newPage)}
-                                        />
-                                    </StyledTableCell>
-                                </TableRow>
-                            )}
-                        </TableFooter>
-                    </Table>
-                </TableContainer>
+                <UserTable
+                    setPage={setPage}
+                    page={page}
+                    totalCount={totalCount}
+                    rows={rows}
+                    handleDelete={handleDelete}
+                    router={router}
+                />
             )}
         </BasePageLayout>
     )
