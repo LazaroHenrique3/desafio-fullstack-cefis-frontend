ebfc3aaa4f5cc8b98b99a50d06bd42cafdf7fdb0
Lazaro Henrique <lazarohenriquefernandeslima@hotmail.com>
2023-11-28 08:04:35 -0300

    feat: Adicionando um componente de button ao BasePageLayout

--- src/app/BasePageLayout.tsx
+++ src/app/BasePageLayout.tsx
@@ -3,17 +3,29 @@ import { Box, Typography } from '@mui/material'
 interface IBasePageLayoutProps {
     children: React.ReactNode,
     title: string,
+    linkButton?: React.ReactNode | undefined,
 }
 
-export const BasePageLayout = ({children, title}: IBasePageLayoutProps) => {
+export const BasePageLayout = ({ children, title, linkButton }: IBasePageLayoutProps) => {
+
+
     return (
         <Box height='100%' padding={5}>
-            <Typography variant='h3' marginBottom={5}>
-                {title}
-            </Typography>
+            <Box display='flex' flexDirection='column' gap={2} marginBottom={2}>
+                <Typography variant='h3' marginBottom={5}>
+                    {title}
+                </Typography>
+
+                {linkButton && (
+                    <Box>
+                        {linkButton}
+                    </Box>
+                )}
+            </Box>
 
             {children}
         </Box>
     )
 }
+
 
