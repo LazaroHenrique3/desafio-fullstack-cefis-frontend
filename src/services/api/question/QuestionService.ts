1af5168799012cb11b02537ee46f99dbb1b09643
Lazaro Henrique <lazarohenriquefernandeslima@hotmail.com>
2023-11-28 18:38:33 -0300

    feat: Criando o service de Question

---
+++ src/services/api/question/QuestionService.ts
@@ -0,0 +1,51 @@
+import { api } from '../axiosConfig'
+import { IDetailCourse } from '../course/CourseService'
+import { IDetailResponse } from '../response/ResponseService'
+
+export interface IListQuestion {
+    id: number
+    idCourse: number,
+    idStudent: number,
+    question_text: string
+    Response: IDetailResponse[]
+}
+
+export interface IDetailQuestion {
+    id: number
+    idCourse: number,
+    idStudent: number,
+    question_text: string
+    Response: IDetailResponse[]
+}
+
+interface ErrorResponse {
+    response: {
+        data?: {
+            errors?: {
+                default?: string
+            }
+        }
+    }
+}
+
+const createQuestion = async (createData: Omit<IDetailCourse, 'id' | 'teacher'>): Promise<IDetailCourse | Error> => {
+
+    try {
+        const { data } = await api.post('/questions/createQuestion', createData)
+
+        if (data) {
+            return data
+        }
+
+        return new Error('Erro ao criar registro.')
+
+    } catch (error) {
+        console.error(error)
+        return new Error((error as ErrorResponse).response?.data?.errors?.default || 'Erro ao criar registro.')
+
+    }
+}
+
+export const QuestionService = {
+    createQuestion
+}
