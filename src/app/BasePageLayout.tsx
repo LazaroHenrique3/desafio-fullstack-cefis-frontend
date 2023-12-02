20530f35a9e7b5e4cd7519a4022f472256737fd4
Lazaro Henrique <lazarohenriquefernandeslima@hotmail.com>
2023-12-02 20:04:06 -0300

    doc: Adicionando comentários explicado as propriedade do BasePageLayout

--- src/app/BasePageLayout.tsx
+++ src/app/BasePageLayout.tsx
@@ -8,14 +8,15 @@ interface IBasePageLayoutProps {
 
 export const BasePageLayout = ({ children, title, linkButton }: IBasePageLayoutProps) => {
 
-
     return (
         <Box height='100%' padding={5}>
             <Box display='flex' flexDirection='column' gap={2} marginBottom={2}>
-                <Typography variant='h3' marginBottom={5}>
+                {/* Texto principal da tela */}
+                <Typography variant='h3' marginBottom={3}>
                     {title}
                 </Typography>
 
+                {/* Link Button que pode ser passado opcionalmente via prop */}
                 {linkButton && (
                     <Box>
                         {linkButton}
