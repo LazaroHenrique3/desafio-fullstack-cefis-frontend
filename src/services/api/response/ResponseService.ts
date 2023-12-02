dffcd800a55f2358faafad687eda11e01f2c5ec5
Lazaro Henrique <lazarohenriquefernandeslima@hotmail.com>
2023-12-02 20:12:28 -0300

    feat: Criando função de listResponse no ResponseService

--- src/services/api/response/ResponseService.ts
+++ src/services/api/response/ResponseService.ts
@@ -1,17 +1,22 @@
+import { EnvironmentValues } from '@/environment'
 import { api } from '../axiosConfig'
 
 export interface IListResponse {
     id: number,
-    idQuestion: number,
+    idResponse: number,
     response_text: string
 }
 
 export interface IDetailResponse {
     id: number,
-    idQuestion: number,
+    idResponse: number,
     response_text: string
 }
 
+type IResponseTotalCount = {
+    data: IListResponse[],
+    totalCount: number
+}
 
 interface ErrorResponse {
     response: {
@@ -19,14 +24,43 @@ interface ErrorResponse {
             errors?: {
                 default?: string
             }
-        }
+        },
+        status?: number
     }
 }
 
-const createResponse = async (idQuestion: number, createData: Omit<IDetailResponse, 'id' >): Promise<IDetailResponse | Error> => {
+const listResponse = async (page = 1, idQuestion: number, orderBy = 'desc' ): Promise<IResponseTotalCount | Error> => {
+    try {
+        const { data, headers } = await api.get(`/responses/listResponseQuestion/${idQuestion}?page=${page}&limit=${EnvironmentValues.LINE_LIMIT}&orderBy=${orderBy}`)
+
+        if (data) {
+            return {
+                data,
+                totalCount: Number(headers['x-total-count'] || EnvironmentValues.LINE_LIMIT)
+            }
+        }
+
+        return new Error('Erro ao listar registros.')
+
+    } catch (error) {
+        console.error(error)
+
+        //Tratando respostas 404, para pode lidar melhor no front
+        if((error as ErrorResponse).response?.status){
+            return {
+                data: [],
+                totalCount: 0
+            }
+        }
+
+        return new Error((error as ErrorResponse).response?.data?.errors?.default || 'Erro ao listar registros.')
+    }
+}
+
+const createResponse = async (idResponse: number, createData: Omit<IDetailResponse, 'id' >): Promise<IDetailResponse | Error> => {
 
     try {
-        const { data } = await api.post(`/responses/createResponse/${idQuestion}`, createData)
+        const { data } = await api.post(`/responses/createResponse/${idResponse}`, createData)
 
         if (data) {
             return data
@@ -42,5 +76,6 @@ const createResponse = async (idQuestion: number, createData: Omit<IDetailRespon
 }
 
 export const ResponseService = {
+    listResponse,
     createResponse
 }
