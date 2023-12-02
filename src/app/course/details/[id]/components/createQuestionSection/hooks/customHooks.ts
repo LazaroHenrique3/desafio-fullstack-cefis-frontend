39490715fc9d921bd650370c69d86c4a29c048d2
Lazaro Henrique <lazarohenriquefernandeslima@hotmail.com>
2023-12-02 20:03:29 -0300

    doc: Adicionando comentários explicado useHandleQuestion

--- src/app/course/details/[id]/components/createQuestionSection/hooks/customHooks.ts
+++ src/app/course/details/[id]/components/createQuestionSection/hooks/customHooks.ts
@@ -17,12 +17,15 @@ interface IUseHandleCourseProps {
     formRef: React.RefObject<FormHandles>
 }
 
+/* A principal motivação de criar esses hooks personalizados é separar o código JS do .tsx ao máximo. Além
+de passar os principais, states e setstate de forma global, já que se eu tivesse o varios handles aqui, todos 
+teriam acesso aos states e handle states que são passados na criação do CustomHook*/
 export const UseHandleQuestion = ({ setIsLoading, setQuestions, questions, idCourse, formRef}: IUseHandleCourseProps) => {
 
     const handleSave = async (data: IFormData) => {
         try {
+            //Validando as informações recebidas do form
             const validateData = await formatValidationCreateSchema.validate(data, { abortEarly: false })
-
             setIsLoading(true)
 
             //Irá retornar a pergunta criada
@@ -34,19 +37,23 @@ export const UseHandleQuestion = ({ setIsLoading, setQuestions, questions, idCou
                 return
             }
 
+            //Criando e setando o novo state com as perguntas atualizadas incluindo a que acabou de ser criada
             const updatedQuestions = [result, ...questions]
+            setQuestions(updatedQuestions)
 
             toast.success('Pergunta salva com sucesso!')
+
+            //Limpando os imputs após a criação bem sucedida
             formRef.current?.setData({
                 question_text: '',
                 idStudent: ''
             })
-            setQuestions(updatedQuestions)
         } catch (errors) {
             const errorsYup: yup.ValidationError = errors as yup.ValidationError
 
             const validationErrors: IVFormErrors = {}
-
+            
+            //Mapeia e passa os erros para o form
             errorsYup.inner.forEach(error => {
                 if (!error.path) return
 
