c1dd04a7c7aa6a35dc4e106b0c9856f0e4cf16c4
Lazaro Henrique <lazarohenriquefernandeslima@hotmail.com>
2023-12-04 15:51:42 -0300

    feat: Criando o provider de session

---
+++ src/providers/sessionProvider.tsx
@@ -0,0 +1,16 @@
+'use client'
+import { SessionProvider } from 'next-auth/react'
+
+interface INextAuthSessionProviderProps {
+    children: React.ReactNode
+}
+
+//Irá tornar público para todas as páginas que o usuário esta logado
+export const NextAuthSessionProvider = ({ children }: INextAuthSessionProviderProps) => {
+    return ( 
+        <SessionProvider>
+            {children}
+        </SessionProvider>
+    )
+}
+ 
