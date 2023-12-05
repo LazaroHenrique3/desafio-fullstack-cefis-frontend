35a126b176ac32abe5eea12c94e858ed95205032
Lazaro Henrique <lazarohenriquefernandeslima@hotmail.com>
2023-12-04 23:24:58 -0300

    feat: Adicionando o Bearer token nas requisições axios

--- src/services/api/axiosConfig/index.ts
+++ src/services/api/axiosConfig/index.ts
@@ -1,7 +1,16 @@
-import axios from 'axios'
+import axios, { AxiosInstance } from 'axios'
+import { getSession } from 'next-auth/react'
 
-const api = axios.create({
+const api: AxiosInstance = axios.create({
     baseURL: process.env.NEXT_PUBLIC_API_URL_BASE,
+})
+
+//Adicionando o token da sessão ao bearer Token do axios
+api.interceptors.request.use(async function (config) {
+    const session = await getSession()
+    config.headers.Authorization = session?.token  ? `Bearer ${session.token}` : ''
+
+    return config
 })
 
 export { api }
