be3e539fd66dc61e7bc0c1ce7e14861b848b4576
Lazaro Henrique <lazarohenriquefernandeslima@hotmail.com>
2023-12-05 15:00:54 -0300

    feat: Adicionando o typeUser na tipagem de dados que são armazenados na sessão

--- next-auth.d.ts
+++ next-auth.d.ts
@@ -1,12 +1,14 @@
 // eslint-disable-next-line 
 import NextAuth from 'next-auth'
+import { TUserRole } from '@/services/api/user/UserService'
 
 declare module 'next-auth' {
     interface Session {
         user: {
-            id: string
+            id: number
             email: string
             name: string
+            typeUser: TUserRole
         },
         token: string
     }
