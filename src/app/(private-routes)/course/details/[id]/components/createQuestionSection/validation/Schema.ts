a9505124eae90a44c6294bf92aa11d20763d541d
Lazaro Henrique <lazarohenriquefernandeslima@hotmail.com>
2023-12-05 15:26:02 -0300

    feat: Removendo o idStudent do schema de create Question, já que agora ele é recebido pelo próprio user da Session

--- src/app/(private-routes)/course/details/[id]/components/createQuestionSection/validation/Schema.ts
+++ src/app/(private-routes)/course/details/[id]/components/createQuestionSection/validation/Schema.ts
@@ -2,11 +2,9 @@ import * as yup from 'yup'
 
 export interface IFormData {
     question_text: string
-    idStudent: number
 }
 
 //Definindo o schema para validação de criação
 export const formatValidationCreateSchema: yup.Schema<IFormData> = yup.object().shape({
     question_text: yup.string().transform(value => (value ? value.trim() : '')).min(3).max(100).required(),
-    idStudent: yup.number().moreThan(0).integer().required()
 })
